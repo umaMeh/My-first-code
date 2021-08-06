5b89d8a22b5456b608772a9006ccf4fb425dd443
umaMeh <umamehta@questglt.org>
2021-08-06 19:39:14 +0530

    first code

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ app.get('/', (req, res) => {
 
 });
 
-app.listen(3000, () => console.log('App listening on port 3000!'));
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => console.log('App listening on port ${port}!'));
